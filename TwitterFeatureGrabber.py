50e4c8b4ab16d369f8dadd5d5109f9c9b6af32ec
Admin <Admin@PF2E62NE-GTH.inc.bytedance.com>
2021-03-25 17:31:02 +0800

    add json line preprocessing method

--- TwitterFeatureGrabber.py
+++ TwitterFeatureGrabber.py
@@ -1,19 +1,31 @@
 import json
+import re
 from nltk.tokenize import TweetTokenizer
+
 
 class FeatureGrabber:
 
     def __init__(self):
         self.tknzr = TweetTokenizer()
 
-    def pre_process(self, data_piece):
-        """ 
-        to do
-        eage case handling
+    def convert2json(self, line):
+        """
 
         @rtype: json 
         """
-        return json.load(data_piece)
+
+        if line[-1] == '\n':
+            line = line[:-1]
+        if line[-2:] == ':[': # first line
+            return None
+        elif line[-2:] == ']}': # finnal line
+            return json.loads(line[:-2])
+        elif line[-2:] == '},': # common twitter line 
+            return json.loads(line[:-1])
+        else:
+            print(line[-5:])
+            raise ValueError('Irregular line found')
+            
 
     def get_tweet(self, json_data):
         return json_data['doc']['text']
@@ -22,25 +34,44 @@ class FeatureGrabber:
         return json_data['doc']['coordinates']['coordinates']
 
     def token_filter(self, tweet):
-        pass
+        # valid punctuation: !,?,'"
+        tokens = self.tknzr.tokenize(tweet)
+        tokens_LC = [t.lower() for t in tokens]
+        pattern = '[a-z]+[\!\,\?\'\"]?'
+        valid_tokens = [t for t in tokens_LC if re.match(pattern, t) != None]
+        return valid_tokens
+    
+    def get_sentiment_score(self, json_data, senti_score_trie) -> int:
+        tweet = self.get_tweet(json_data)
+        valid_tokens = self.token_filter(tweet)
+        total_score = 0
+        for t in valid_tokens:
+            if int(senti_score_trie.find(t)) > 0:
+                print(t)
+            total_score += int(senti_score_trie.find(t))
+        return total_score
 
-if __name__ == '__main__':
+
+# if __name__ == '__main__':
     # """ TEST """
     # print('test begin')
     # featGrabber = FeatureGrabber()
     # path = r'files/tinyTwitter.json'
     # with open(path, encoding='utf-8') as f:
-    #     data = f.readlines()[1]
-    #     data = data[:-2]
-    # # print(data)
+        # data = f.readlines()[1]
+        # print(data[-1] == '\n')
+        # print(data[-2])
+        # data = data[:-2]
+
+        # first_line = f.readline()
+        # print(first_line)
+        # print(first_line[-1] == '\n')
+        # print(first_line[-2] == '[')
+    # print(data)
     # j = json.loads(data)
     # print(featGrabber.get_coordinates(j))
-    # print(featGrabber.get_tweet(j))
-
-
-
-
-
-    
-
-
+    # tweet = featGrabber.get_tweet(j)
+    # tweet = 'I\'m @hello, hello! hello\' hello\" hello. hello? \
+    # @Hello, heLlo! helLo\' HELlo\" heLlo. helLo?'
+    # print(tweet)
+    # print(featGrabber.token_filter(tweet))
--- main.py
+++ main.py
@@ -3,14 +3,38 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+from utils.Trie import Trie
+from TwitterFeatureGrabber import FeatureGrabber
+import csv
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    """ TEST---Tokenizer and Trie """
+    senti_score_trie = Trie()
+    feat_grabber = FeatureGrabber()
+    with open(r'.\files\AFINN.txt') as f:
+        reader = csv.reader(f, delimiter="\t")
+        d = list(reader)
+        for word, score in d:
+            senti_score_trie.add(word, score)
+    # print(senti_score_trie.find('abandon'))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with open(r'.\files\tinyTwitter.json', encoding='utf-8') as f:
+        lines = f.readlines()
+    # print(len(lines))
+    # print('test begin')
+    i = 0
+    for line in lines:
+        i += 1
+        print('_' * 30, i, '_' * 30)
+        try:
+            json_data = feat_grabber.convert2json(line)
+            if json_data:
+                print(feat_grabber.get_tweet(json_data))
+                print('total score', feat_grabber.get_sentiment_score(json_data, senti_score_trie))
+        except TypeError as e:
+            # print(line)
+            # print(json_data)
+            # break
+            print(e)
+            pass
